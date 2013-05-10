163c07fdbc073ae384449c4138d6af75d821bf4d
Ben Han <benhan2@gmail.com>
2013-05-09 23:16:46 -0400

    Edit home page and layout elements

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,37 +1,37 @@
 class Ability
 	include CanCan::Ability
 
-	  def initialize(user)
-      @user = user || User.new # for guest
-      @user.roles.each { |role| send(role) }
+  def initialize(user)
+    @user = user || User.new # for guest
+    @user.roles.each { |role| send(role) }
 
-      if @user.roles.size == 0
-        can :read, :all #for guest without roles
-      end
+    if @user.roles.size == 0
+      can :read, :all #for guest without roles
     end
+  end
 
-    def admin
-      toh_staff
-    end
+  def admin
+    toh_staff
+  end
 
-    def toh_staff
-      coordinator
-    end
+  def toh_staff
+    coordinator
+  end
 
-    def coordinator
-      volunteer
-      can :manage, :all
-    end
+  def coordinator
+    volunteer
+    can :manage, :all
+  end
 
-    def volunteer
-      can :read, :all
-    end
+  def volunteer
+    can :read, :all
+  end
 
-    def guest
-      can :read, :all
-    end
+  def guest
+    can :read, :all
+  end
 
-    def banned
-      cannot :read, :all
-    end
+  def banned
+    cannot :read, :all
+  end
 end
