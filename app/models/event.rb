e90ec659a2de8fc760e4efd2b9fa0782b60b11ea
Ben Han <benhan2@gmail.com>
2013-05-14 17:42:05 -0400

    Add Events scaffold.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ActiveRecord::Base
+	# Accessible attributes
   attr_accessible :end_date, :name, :start_date
+
+  # Validations
+  validates :name, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+
+  # Relationships
+
 end
